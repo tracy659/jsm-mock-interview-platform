00e23a338c1f0955fbc51c15b875a37750d95344
Tracy Al khoury <alkhourytraicy@gmail.com>
2025-04-03 17:13:24 +0300

    show feedback

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -6,6 +6,7 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { vapi } from "@/lib/vapi.sdk";
 import { interviewer } from "@/constants";
+import { createFeedback } from "@/lib/actions/general.action";
 
 enum CallStatus {
     INACTIVE = "INACTIVE",
@@ -78,10 +79,12 @@ const Agent = ({userName, userId, type, interviewId, questions}: AgentProps) => 
         const handleGenerateFeedback = async (messages: SavedMessage[]) => {
             console.log("handleGenerateFeedback");
 
-           const {success, id} = {
-               success:true,
-               id: 'feedback-id',
-           }
+            const { success, feedbackId: id } = await createFeedback({
+                interviewId: interviewId!,
+                userId: userId!,
+                transcript: messages,
+
+            });
 
             if (success && id) {
                 router.push(`/interview/${interviewId}/feedback`);
--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -4,8 +4,9 @@ import Link from "next/link";
 import { cn, getRandomInterviewCover } from "@/lib/utils";
 import {Button} from "@/components/ui/button";
 import DisplayTechIcons from "@/components/DisplayTechIcons";
+import { getFeedbackByInterviewId } from "@/lib/actions/general.action";
 
-const InterviewCard = ({
+const InterviewCard = async ({
                            id,
                            userId,
                            role,
@@ -13,7 +14,14 @@ const InterviewCard = ({
                            techstack,
                            createdAt,
                        }: InterviewCardProps) => {
-    const feedback = null as Feedback | null;
+    const feedback =
+        userId && id
+            ? await getFeedbackByInterviewId({
+                interviewId:id,
+                userId,
+            })
+            : null;
+
     const normalizedType = /mix/gi.test(type) ? "Mixed" : type;
     const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now()).format("MMM D, YYYY");
 
