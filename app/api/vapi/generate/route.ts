425227ddb35a8426d04fa4f503d8eb45660f2d26
Tracy Al khoury <alkhourytraicy@gmail.com>
2026-01-05 15:25:52 +0200

    Revert "small fixes"
    This reverts commit 88571ebcec7afc1ff9d76ff397d8a3c3493e45b8.

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -5,12 +5,12 @@ import { db } from "@/firebase/admin";
 import { getRandomInterviewCover } from "@/lib/utils";
 
 export async function POST(request: Request) {
-  const { type, role, level, techstack, amount, userid } = await request.json();
+    const { type, role, level, techstack, amount, userid } = await request.json();
 
-  try {
-    const { text: questions } = await generateText({
-      model: google("gemini-2.0-flash-001"),
-      prompt: `Prepare questions for a job interview.
+    try {
+        const { text: questions } = await generateText({
+            model: google("gemini-2.0-flash-001"),
+            prompt: `Prepare questions for a job interview.
         The job role is ${role}.
         The job experience level is ${level}.
         The tech stack used in the job is: ${techstack}.
@@ -23,30 +23,30 @@ export async function POST(request: Request) {
         
         Thank you! <3
     `,
-    });
+        });
 
-    const interview = {
-      role: role,
-      type: type,
-      level: level,
-      techstack: techstack.split(","),
-      questions: JSON.parse(questions),
-      userId: userid,
-      finalized: true,
-      coverImage: getRandomInterviewCover(),
-      createdAt: new Date().toISOString(),
-    };
+        const interview = {
+            role: role,
+            type: type,
+            level: level,
+            techstack: techstack.split(","),
+            questions: JSON.parse(questions),
+            userId: userid,
+            finalized: true,
+            coverImage: getRandomInterviewCover(),
+            createdAt: new Date().toISOString(),
+        };
 
-    await db.collection("interviews").add(interview);
+        await db.collection("interviews").add(interview);
 
-    return Response.json({ success: true }, { status: 200 });
-  } catch (error) {
-    console.error("Error:", error);
-    return Response.json({ success: false, error: error }, { status: 500 });
-  }
+        return Response.json({ success: true }, { status: 200 });
+    } catch (error) {
+        console.error("Error:", error);
+        return Response.json({ success: false, error: error }, { status: 500 });
+    }
 }
 
 export async function GET() {
-  return Response.json({ success: true, data: "Thank you!" }, { status: 200 });
+    return Response.json({ success: true, data: "Thank you!" }, { status: 200 });
 }
 // test branch tracy
